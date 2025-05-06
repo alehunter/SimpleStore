141bd943776bf70d2f8b6c937a3ee2f1d191c467
Alejandro Hunter <57914534+alehunter@users.noreply.github.com>
2025-05-06 13:48:30 -0400

    /simplestore/all_items now lists all of the items in the db

--- djangotutorial/simplestore/urls.py
+++ djangotutorial/simplestore/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("all_items/", views.all_items, name="all_items"),
 ]
--- djangotutorial/simplestore/views.py
+++ djangotutorial/simplestore/views.py
@@ -1,5 +1,18 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Item
 
 def index(request):
     return HttpResponse("Welcome to the simple store!")
 
+
+##this method is a generic method that will show all the records 
+##for a given database model 
+# def all_records(request):
+
+
+def all_items(request):
+    itemList = Item.objects.all()
+    context = {"itemList": itemList}
+    return render(request, "simplestore/all_items.html", context)
